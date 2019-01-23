a2d15f7cf84881ab0632f533f4be308ee2114989
rodrigo-xavier <rodrigomatiasxavier@gmail.com>
2019-01-23 00:05:50 -0200

    Alterando nome dos arquivos e adicionando funcionalidade para escolher portas do arduino e video automaticamente

--- src/play.m
+++ src/play.m
@@ -1,21 +1,49 @@
-% Conexão com o arduino e com o vídeo
-arduino_port = 'COM6';
-vid_port = 2; % vid_port pode variar (1 ou 2)
-
 % deconecta o arduino se estiver conectado
 if exist('conexao','var') == 1
     fclose(conexao);
 end
-
-conexao = serial(arduino_port);
-fopen(conexao);
 
 % deleta o video se ele existir
 if exist('vid','var') == 1
     delete(vid);
 end
 
-vid = videoinput('winvideo', vid_port, 'I420_640x480');
+i = 0;
+
+% Tenta iniciar conexão com arduino nas 20 primeiras portas
+while(i <= 20)
+    port_name = 'COM';
+    port_number = int2str(i);
+    arduino_port = strcat(port_name, port_number);
+
+    try
+        conexao = serial(arduino_port);
+        fopen(conexao);
+        break;
+    catch
+        string = ['Porta COM', i, ' Falhou'];
+        disp(string)
+    end
+
+    i = i + 1;
+end
+
+i = 0;
+
+% Tenta iniciar conexão com video nas 2 primeiras portas
+while(i <= 2)
+    vid_port = i; % vid_port pode variar (1 ou 2)
+
+    try
+        vid = videoinput('winvideo', vid_port, 'I420_640x480');
+        break;
+    catch
+        string = ['Porta VID', i, ' Falhou'];
+        disp(string)
+    end
+
+    i = i + 1;
+end
 
 hvpc = vision.VideoPlayer;   %create video player object
 
@@ -48,3 +76,4 @@ while true
     
     imagesc(imgO);
 end
+    
