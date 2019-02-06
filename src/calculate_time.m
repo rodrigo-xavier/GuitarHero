28e4635c26bcbaa79ddad6484476360920a12eef
Alec <ryo.alec@gmail.com>
2019-02-05 22:16:24 -0200

    Funcao pra calcular o tempo do rastro ate o aperto e um bloco de notas com esses tempos

---
+++ src/calculate_time.m
@@ -0,0 +1,55 @@
+% Encerra conexão com galileo se estiver conectado
+if exist('galileo','var') == true
+    fclose(galileo);
+end
+
+% Encerra conexão com video se estiver conectado
+if exist('vid','var') == true
+    delete(vid);
+end
+
+[vid, galileo] = connect_devices();
+configure_video(vid);
+from_one_red_note(vid, galileo);
+
+function from_one_red_note(vid, galileo)
+        
+    while true
+        % get image from camera
+        imgO = getdata(vid,1,'uint8');
+
+        if (imgO(293,275,1) >= 175 && imgO(311,274,1) <= 255)
+            tic;
+        end
+    
+        if (imgO(412,248,1) >= 175 && imgO(412,248,1) <= 255)
+            time = toc
+            beep;
+        end
+
+        imagesc(imgO);
+
+    end
+
+end
+
+function from_red_rastro(vid, galileo)
+
+    while true
+        % get image from camera
+        imgO = getdata(vid,1,'uint8');
+
+        if (imgO(281,278,1) >= 175 && imgO(281,278,1) <= 255)
+            tic;
+        end
+    
+        if (imgO(412,248,1) >= 175 && imgO(412,248,1) <= 255)
+            time = toc
+            beep;
+        end
+
+        imagesc(imgO);
+
+    end
+
+end
