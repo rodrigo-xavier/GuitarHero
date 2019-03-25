509295f12224926c373b2664f0855c07624a235a
Cesar Carvalho <cesaraugc@gmail.com>
2019-03-25 20:12:04 -0300

    Adiciona código para jogar em todas as cores

--- arduino/solenoide_controller/solenoide_controller.ino
+++ arduino/solenoide_controller/solenoide_controller.ino
@@ -11,12 +11,12 @@ class SimpleState{
     unsigned long previousMillis;
     
   public:
-    SimpleState(unsigned long prev, unsigned long off){
-      pin=3;
+    SimpleState(int pin, unsigned long prev, unsigned long off){
+      this->pin=pin;
       pinMode(pin, OUTPUT);
-      offTime = off;
-      finished = false;
-      previousMillis = prev;
+      this->offTime = off;
+      this->finished = false;
+      this->previousMillis = prev;
     }
 
     void Update(){
@@ -41,15 +41,15 @@ class TraceState{
     unsigned long previousMillisFree;
 
   public:
-    TraceState(unsigned long prev, unsigned long off){
-      pin=3;
+    TraceState(int pin, unsigned long prev, unsigned long off){
+      this->pin=pin;
       pinMode(pin, OUTPUT);
-      finished = false;
-      pressed = false;
-      previousMillis = prev;
-      previousMillisFree = prev;
-      soltar = false;
-      offTime = off;
+      this->finished = false;
+      this->pressed = false;
+      this->previousMillis = prev;
+      this->previousMillisFree = prev;
+      this->soltar = false;
+      this->offTime = off;
     }
 
     void Update(){
@@ -74,31 +74,82 @@ class TraceState{
     }
 };
 
-int L1 = 3;
+
 unsigned long offtime_simple = 0;
 unsigned long offtime_rastro = 0;
+
+#define L1_PIN 3
+#define L2_PIN 2
+#define R1_PIN 4
+#define R2_PIN 5
+#define X_PIN 6
+
 unsigned char incomingByte = '\0';
 
-int freeStates[N_SIMPLE_STATES]={1,1,1,1,1,1,1,1,1,1};
-SimpleState *simpleStates[N_SIMPLE_STATES];
-TraceState *traceStates[N_TRACE_STATES];
-QueueArray <int> traceQueue;
+// Red States
+SimpleState *redSimpleStates[N_SIMPLE_STATES];
+TraceState *redTraceStates[N_TRACE_STATES];
+QueueArray <int> redTraceQueue;
 
-int ind = 0, first_item;
+// Green States
+SimpleState *greenSimpleStates[N_SIMPLE_STATES];
+TraceState *greenTraceStates[N_TRACE_STATES];
+QueueArray <int> greenTraceQueue;
+
+// Yellow States
+SimpleState *yellowSimpleStates[N_SIMPLE_STATES];
+TraceState *yellowTraceStates[N_TRACE_STATES];
+QueueArray <int> yellowTraceQueue;
+
+// Blue States
+SimpleState *blueSimpleStates[N_SIMPLE_STATES];
+TraceState *blueTraceStates[N_TRACE_STATES];
+QueueArray <int> blueTraceQueue;
+
+// Orange States
+SimpleState *orangeSimpleStates[N_SIMPLE_STATES];
+TraceState *orangeTraceStates[N_TRACE_STATES];
+QueueArray <int> orangeTraceQueue;
+
+int ind=0, first_item=0;
 
 void setup() {
   Serial.begin(115200);
   
-  // Inicializa os estados
+  // Inicializa os estados simples
   for(int i=0; i<N_SIMPLE_STATES; i++){
-    freeStates[i] = 1;
-    simpleStates[i] = new SimpleState(millis(), offtime_simple);
-    simpleStates[i]->finished = true;
+    redSimpleStates[i] = new SimpleState(L1_PIN, millis(), offtime_simple);
+    redSimpleStates[i]->finished = true;
+
+    greenSimpleStates[i] = new SimpleState(L2_PIN, millis(), offtime_simple);
+    greenSimpleStates[i]->finished = true;
+
+    yellowSimpleStates[i] = new SimpleState(R1_PIN, millis(), offtime_simple);
+    yellowSimpleStates[i]->finished = true;
+
+    blueSimpleStates[i] = new SimpleState(R2_PIN, millis(), offtime_simple);
+    blueSimpleStates[i]->finished = true;
+
+    orangeSimpleStates[i] = new SimpleState(X_PIN, millis(), offtime_simple);
+    orangeSimpleStates[i]->finished = true;    
   }
 
+  // Inicializa os estados de rastros
   for(int i=0; i<N_TRACE_STATES; i++){
-    traceStates[i] = new TraceState(millis(), offtime_rastro);
-    traceStates[i]->finished = true;
+    redTraceStates[i] = new TraceState(L1_PIN, millis(), offtime_rastro);
+    redTraceStates[i]->finished = true;
+
+    greenTraceStates[i] = new TraceState(L2_PIN, millis(), offtime_rastro);
+    greenTraceStates[i]->finished = true;
+
+    yellowTraceStates[i] = new TraceState(R1_PIN, millis(), offtime_rastro);
+    yellowTraceStates[i]->finished = true;
+
+    blueTraceStates[i] = new TraceState(R2_PIN, millis(), offtime_rastro);
+    blueTraceStates[i]->finished = true;
+
+    orangeTraceStates[i] = new TraceState(X_PIN, millis(), offtime_rastro);
+    orangeTraceStates[i]->finished = true;
   }
 
   delay(1000);
@@ -110,30 +161,144 @@ void loop(){
     
     // Estado simples: aperta e solta automaticamente 
     // o mais rápido possível
+
+    // Red (L1_PIN)
     if(incomingByte == char(100)){
-      ind=get_simpleStates_index();
-      simpleStates[ind] = new SimpleState(millis(), offtime_simple);
+      ind=get_simpleStates_index('R');
+      redSimpleStates[ind] = new SimpleState(L1_PIN, millis(), offtime_simple);
       incomingByte = '\0';
     }
+
+    // Green (L2_PIN)
+    if(incomingByte == char(110)){
+      ind=get_simpleStates_index('G');
+      greenSimpleStates[ind] = new SimpleState(L2_PIN, millis(), offtime_simple);
+      incomingByte = '\0';
+    }
+
+    // Yellow (R1_PIN)
+    if(incomingByte == char(120)){
+      ind=get_simpleStates_index('Y');
+      yellowSimpleStates[ind] = new SimpleState(R1_PIN, millis(), offtime_simple);
+      incomingByte = '\0';
+    }
+
+    // Blue (R1_PIN)
+    if(incomingByte == char(130)){
+      ind=get_simpleStates_index('B');
+      blueSimpleStates[ind] = new SimpleState(R2_PIN, millis(), offtime_simple);
+      incomingByte = '\0';
+    }
+
+    // Orange (X_PIN)
+    if(incomingByte == char(140)){
+      ind=get_simpleStates_index('O');
+      orangeSimpleStates[ind] = new SimpleState(X_PIN, millis(), offtime_simple);
+      incomingByte = '\0';
+    }
+
+
+    // ----------------------------------------------------- //
 
     // Estado de rastro: Aperta sem soltar
+    // Red (L1_PIN)
     if(incomingByte == char(101)){
-      ind = get_traceStates_index();
-      traceStates[ind] = new TraceState(millis(), offtime_rastro);
-      traceQueue.push(ind);
+
+      ind = get_traceStates_index('R');
+      redTraceStates[ind] = new TraceState(L1_PIN, millis(), offtime_rastro);
+      redTraceQueue.push(ind);
       incomingByte = '\0';
     }
 
+    // Green (L2_PIN)
+    if(incomingByte == char(111)){
+      ind = get_traceStates_index('G');
+      greenTraceStates[ind] = new TraceState(L2_PIN, millis(), offtime_rastro);
+      greenTraceQueue.push(ind);
+      incomingByte = '\0';
+    }
+
+    // Yellow (R1_PIN)
+    if(incomingByte == char(121)){
+      ind = get_traceStates_index('Y');
+      yellowTraceStates[ind] = new TraceState(R1_PIN, millis(), offtime_rastro);
+      yellowTraceQueue.push(ind);
+      incomingByte = '\0';
+    }
+
+    // Blue (R2_PIN)
+    if(incomingByte == char(131)){
+      ind = get_traceStates_index('B');
+      blueTraceStates[ind] = new TraceState(R2_PIN, millis(), offtime_rastro);
+      blueTraceQueue.push(ind);
+      incomingByte = '\0';
+    }
+
+    // Orange (X_PIN)
+    if(incomingByte == char(141)){
+      ind = get_traceStates_index('O');
+      orangeTraceStates[ind] = new TraceState(X_PIN, millis(), offtime_rastro);
+      orangeTraceQueue.push(ind);
+      incomingByte = '\0';
+    }
+
+
+    // ------ //
+
     // Estado de rastro: Solta
-    if(incomingByte == char(102) && !traceQueue.isEmpty()){
-      first_item = traceQueue.front();
-      if(!traceStates[first_item]->finished){
-        ind = traceQueue.pop();
-        traceStates[ind]->Soltar(millis(),offtime_simple);
+
+    // Red (L1_PIN)
+    if(incomingByte == char(102) && !redTraceQueue.isEmpty()){
+      first_item = redTraceQueue.front();
+      if(!redTraceStates[first_item]->finished){
+        ind = redTraceQueue.pop();
+        redTraceStates[ind]->Soltar(millis(), offtime_simple);
+        incomingByte = '\0';
+      }
+    }
+    
+    // Green (R1_PIN)
+    if(incomingByte == char(112) && !greenTraceQueue.isEmpty()){
+      first_item = greenTraceQueue.front();
+      if(!greenTraceStates[first_item]->finished){
+        ind = greenTraceQueue.pop();
+        greenTraceStates[ind]->Soltar(millis(), offtime_simple);
         incomingByte = '\0';
       }
     }
 
+    // Yellow (R1_PIN)
+    if(incomingByte == char(122) && !yellowTraceQueue.isEmpty()){
+      first_item = yellowTraceQueue.front();
+      if(!yellowTraceStates[first_item]->finished){
+        ind = yellowTraceQueue.pop();
+        yellowTraceStates[ind]->Soltar(millis(), offtime_simple);
+        incomingByte = '\0';
+      }
+    }
+
+    // Blue (R2_PIN)
+    if(incomingByte == char(132) && !blueTraceQueue.isEmpty()){
+      first_item = blueTraceQueue.front();
+      if(!blueTraceStates[first_item]->finished){
+        ind = blueTraceQueue.pop();
+        blueTraceStates[ind]->Soltar(millis(), offtime_simple);
+        incomingByte = '\0';
+      }
+    }
+
+    // Orange (X_PIN)
+    if(incomingByte == char(142) && !orangeTraceQueue.isEmpty()){
+      first_item = orangeTraceQueue.front();
+      if(!orangeTraceStates[first_item]->finished){
+        ind = orangeTraceQueue.pop();
+        orangeTraceStates[ind]->Soltar(millis(), offtime_simple);
+        incomingByte = '\0';
+      }
+    }
+
+
+    
     // Obtem o tempo de offtime de nota simples
     if(incomingByte == char(90)){
       offtime_simple = 0;
@@ -145,38 +310,115 @@ void loop(){
       offtime_rastro = 0;
       getRastroTime();
     }
-    
   }
 
   checkAllOnStates();
   
 }
 
-int get_simpleStates_index(){
-  for(int i=0; i<N_SIMPLE_STATES; i++){
-    if(simpleStates[i]->finished)
-      return i;
+int get_simpleStates_index(char color){
+  // Função que retorna o índice do array
+  // a ser utilizado para alocar o estado 
+
+  if(color == 'R'){
+    for(int i=0; i<N_SIMPLE_STATES; i++){
+      if(redSimpleStates[i]->finished)
+        return i;
+    }
   }
+  else if(color == 'G'){
+    for(int i=0; i<N_SIMPLE_STATES; i++){
+      if(greenSimpleStates[i]->finished)
+        return i;
+    }
+  }
+  else if(color == 'Y'){
+    for(int i=0; i<N_SIMPLE_STATES; i++){
+      if(yellowSimpleStates[i]->finished)
+        return i;
+    }
+  }
+  else if(color == 'B'){
+    for(int i=0; i<N_SIMPLE_STATES; i++){
+      if(blueSimpleStates[i]->finished)
+        return i;
+    }
+  }
+  else if(color == 'O'){
+    for(int i=0; i<N_SIMPLE_STATES; i++){
+      if(orangeSimpleStates[i]->finished)
+        return i;
+    }
+  }
+
   return -1;
 }
 
-int get_traceStates_index(){
-  for(int i=0; i<N_TRACE_STATES; i++){
-    if(traceStates[i]->finished)
-      return i;
+int get_traceStates_index(char color){
+  // Função que retorna o índice do array
+  // a ser utilizado para alocar o estado 
+
+  if(color == 'R'){
+    for(int i=0; i<N_TRACE_STATES; i++){
+      if(redTraceStates[i]->finished)
+        return i;
+    }
   }
+  else if(color == 'G'){
+    for(int i=0; i<N_TRACE_STATES; i++){
+      if(greenTraceStates[i]->finished)
+        return i;
+    }
+  }
+  else if(color == 'Y'){
+    for(int i=0; i<N_TRACE_STATES; i++){
+      if(yellowTraceStates[i]->finished)
+        return i;
+    }
+  }
+  else if(color == 'B'){
+    for(int i=0; i<N_TRACE_STATES; i++){
+      if(blueTraceStates[i]->finished)
+        return i;
+    }
+  }
+  else if(color == 'O'){
+    for(int i=0; i<N_TRACE_STATES; i++){
+      if(orangeTraceStates[i]->finished)
+        return i;
+    }
+  }
+
   return -1;
 }
 
 void checkAllOnStates(){
+  // Verifica todos os estados que estão ativos
+
   for(int i=0; i<N_SIMPLE_STATES; i++){
-    if(!simpleStates[i]->finished) // se nao foi finished ainda
-      simpleStates[i]->Update();
+    if(!redSimpleStates[i]->finished) // se não foi finalizada ainda
+      redSimpleStates[i]->Update();
+    if(!greenSimpleStates[i]->finished)
+      greenSimpleStates[i]->Update();
+    if(!yellowSimpleStates[i]->finished) 
+      yellowSimpleStates[i]->Update();
+    if(!blueSimpleStates[i]->finished) 
+      blueSimpleStates[i]->Update();
+    if(!orangeSimpleStates[i]->finished)
+      orangeSimpleStates[i]->Update();
     
 
     if(i < N_TRACE_STATES){
-      if(!traceStates[i]->finished)
-        traceStates[i]->Update();
+      if(!redTraceStates[i]->finished)
+        redTraceStates[i]->Update();
+      if(!greenTraceStates[i]->finished)
+        greenTraceStates[i]->Update();
+      if(!yellowTraceStates[i]->finished)
+        yellowTraceStates[i]->Update();
+      if(!blueTraceStates[i]->finished)
+        blueTraceStates[i]->Update();
+      if(!orangeTraceStates[i]->finished)
+        orangeTraceStates[i]->Update();
     }
   }
 }
--- src/press_buttons.m
+++ src/press_buttons.m
@@ -33,6 +33,20 @@ function press_buttons(vid, galileo)
 
     % acoes
     APERTA_E_SOLTA_RED = char(100);
+    APERTA_SEM_SOLTAR_RED = char(101);
+    SOLTA_RED = char(102);
+    APERTA_E_SOLTA_GREEN = char(110);
+    APERTA_SEM_SOLTAR_GREEN = char(111);
+    SOLTA_GREEN = char(112);
+    APERTA_E_SOLTA_YELLOW = char(120);
+    APERTA_SEM_SOLTAR_YELLOW = char(121);
+    SOLTA_YELLOW = char(122);
+    APERTA_E_SOLTA_BLUE = char(130);
+    APERTA_SEM_SOLTAR_BLUE = char(131);
+    SOLTA_BLUE = char(132);
+    APERTA_E_SOLTA_ORANGE = char(140);
+    APERTA_SEM_SOLTAR_ORANGE = char(141);
+    SOLTA_ORANGE = char(142);
 
     % tempo
     [tempo_simples, tempo_espera] = chose_times(nivel);
@@ -54,8 +68,13 @@ function press_buttons(vid, galileo)
     keys = {'green', 'red', 'yellow', 'blue', 'orange'};
     values = [uint64(0) uint64(0) uint64(0) uint64(0) uint64(0)];
     holding_times = containers.Map(keys, values);
-
+    
+    green_time = tic;
     red_time = tic;
+    yellow_time = tic;
+    blue_time = tic;
+    orange_time = tic;
+
     preview(vid);
     while true
         % get image from camera
@@ -76,10 +95,12 @@ function press_buttons(vid, galileo)
         [holding_buttons, holding_times] = rastro_detection(galileo, imgO, holding_buttons, holding_times);
         
         %detect green
-        if( greenPixel >= green_min && greenPixel <= green_max && ...
-            ~holding_buttons('green') )
-            % do something
+        if(greenPixel >= green_min && greenPixel <= green_max &&  ...
+            ~holding_buttons('green') && toc(green_time) > tempo_espera)
+            fprintf(galileo,'%c', APERTA_E_SOLTA_GREEN);
+            green_time = tic;
         end
+
 
         %detect red   
         if( redPixel >= red_min && redPixel <= red_max && ~holding_buttons('red') ...
@@ -89,26 +110,29 @@ function press_buttons(vid, galileo)
         end
 
         %detect yellow
-        if( yellowPixelR >= yellowR_min && yellowPixelR <= yellowR_max && ...
-            yellowPixelG >= yellowG_min && yellowPixelG <= yellowG_max && ...
-            ~holding_buttons('yellow'))
-                % do something
+        if(yellowPixelR >= yellowR_min && yellowPixelR <= yellowR_max && ...
+           yellowPixelG >= yellowG_min && yellowPixelG <= yellowG_max && ...
+           toc(yellow_time) > tempo_espera &&  ~holding_buttons('yellow'))
+           fprintf(galileo,'%c', APERTA_E_SOLTA_YELLOW);
+           yellow_time = tic;
         end
 
         %detect blue
-        if( bluePixelG >= blueG_min && bluePixelG <= blueG_max && ...
-            bluePixelB >= blueB_min && bluePixelB <= blueB_max && ...
-            ~holding_buttons('blue'))
-            % do something
+        if(bluePixelG >= blueG_min && bluePixelG <= blueG_max && ...
+           bluePixelB >= blueB_min && bluePixelB <= blueB_max && ...
+           toc(blue_time) > tempo_espera &&  ~holding_buttons('blue'))
+           fprintf(galileo,'%c', APERTA_E_SOLTA_BLUE);
+           blue_time = tic;
         end
 
         %detect orange
-        if( orangePixelR >= orangeR_min && orangePixelR <= orangeR_max && ...
-            orangePixelG >= orangeG_min && orangePixelG <= orangeG_max && ...
-            ~holding_buttons('orange'))
-            % do something
+        if(orangePixelR >= orangeR_min && orangePixelR <= orangeR_max && ...
+           orangePixelG >= orangeG_min && orangePixelG <= orangeG_max && ...
+           toc(orange_time) > tempo_espera &&  ~holding_buttons('orange'))
+           fprintf(galileo,'%c', APERTA_E_SOLTA_ORANGE);
+           orange_time = tic;
         end
-
+        
         if(debug)
             % Colore de verde os pixels que estão sendo utilizados
 
@@ -177,9 +201,8 @@ function press_buttons(vid, galileo)
             imgO(281,278,R) = 0;
             imgO(281,278,G) = 255;
             imgO(281,278,B) = 0;
-            
+
             imagesc(imgO);
         end
-
     end
 end
