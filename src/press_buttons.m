f230975f496bd87c2532ac1fe4b7b18fd56ad599
rodrigo-xavier <rodrigomatiasxavier@gmail.com>
2019-04-02 17:22:13 -0300

    revertendo alterações de debug para criar nova issue

--- src/press_buttons.m
+++ src/press_buttons.m
@@ -98,7 +98,78 @@ function press_buttons(vid, galileo)
            orangePixelG >= orangeG_min && orangePixelG <= orangeG_max )
             % do something
         end
+        
+        if(debug)
+            % Colore de verde os pixels que estão sendo utilizados
 
-        debug(1, imgO)
+            % Simple Green
+            imgO(312,230,R) = 0;
+            imgO(312,230,G) = 255;
+            imgO(312,230,B) = 0;
+
+            % Simple Red
+            imgO(311,274,R) = 0;
+            imgO(311,274,G) = 255;
+            imgO(311,274,B) = 0;
+
+            % Simple Yellow
+            imgO(312,311,R) = 0;
+            imgO(312,311,G) = 255;
+            imgO(312,311,B) = 0;
+
+            % Simple Blue
+            imgO(312,354,R) = 0;
+            imgO(312,354,G) = 255;
+            imgO(312,354,B) = 0;
+
+            % Simple Orange
+            imgO(311,395,R) = 0;
+            imgO(311,395,G) = 255;
+            imgO(311,395,B) = 0;
+
+            % Rastro Vermelho
+            imgO(293,275,R) = 0;
+            imgO(293,275,G) = 255;
+            imgO(293,275,B) = 0;
+            imgO(292,275,R) = 0;
+            imgO(292,275,G) = 255;
+            imgO(292,275,B) = 0;
+            imgO(291,275,R) = 0;
+            imgO(291,275,G) = 255;
+            imgO(291,275,B) = 0;
+            imgO(290,276,R) = 0;
+            imgO(290,276,G) = 255;
+            imgO(290,276,B) = 0;
+            imgO(289,276,R) = 0;
+            imgO(289,276,G) = 255;
+            imgO(289,276,B) = 0;
+            imgO(288,276,R) = 0;
+            imgO(288,276,G) = 255;
+            imgO(288,276,B) = 0;
+            imgO(287,276,R) = 0;
+            imgO(287,276,G) = 255;
+            imgO(287,276,B) = 0;
+            imgO(286,276,R) = 0;
+            imgO(286,276,G) = 255;
+            imgO(286,276,B) = 0;
+            imgO(285,277,R) = 0;
+            imgO(285,277,G) = 255;
+            imgO(285,277,B) = 0;
+            imgO(284,277,R) = 0;
+            imgO(284,277,G) = 255;
+            imgO(284,277,B) = 0;
+            imgO(283,277,R) = 0;
+            imgO(283,277,G) = 255;
+            imgO(283,277,B) = 0;
+            imgO(282,277,R) = 0;
+            imgO(282,277,G) = 255;
+            imgO(282,277,B) = 0;
+            imgO(281,278,R) = 0;
+            imgO(281,278,G) = 255;
+            imgO(281,278,B) = 0;
+            
+            imagesc(imgO);
+        end
+
     end
 end
