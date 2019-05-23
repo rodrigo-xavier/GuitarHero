5d9e63ae01e45db6057d1dba02c07337fec27f2c
rodrigo-xavier <rodrigomatiasxavier@gmail.com>
2019-05-23 20:26:20 -0300

    módulo debug renomeado para depurate no commit anterior, módulo debug pronto. Falta apenas testar

--- src/press_buttons/press_buttons_red.m
+++ src/press_buttons/press_buttons_red.m
@@ -4,6 +4,8 @@ function press_buttons_red(vid, galileo)
 
     % acoes
     APERTA_E_SOLTA_RED = char(100);
+    APERTA_SEM_SOLTAR_RED = char(101);
+    SOLTA_RED = char(102);
     
     [tempo_simples, tempo_rastro] = detect_times(vid);
     tempo_espera = tempo_rastro - tempo_simples;
@@ -25,7 +27,43 @@ function press_buttons_red(vid, galileo)
         imgO = getdata(vid,1,'uint8');
         redPixel = imgO(311,274,R);
         
-        [holding_buttons, holding_times] = rastro_play(galileo, imgO, holding_buttons, holding_times);
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% rastro_play %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+        if( ~holding_buttons('red') && ...
+            (imgO(293,275,R) >= red_min && imgO(293,275,R) <= red_max) && ...
+            (imgO(292,275,R) >= red_min && imgO(292,275,R) <= red_max) && ...
+            (imgO(291,275,R) >= red_min && imgO(291,275,R) <= red_max) && ...
+            (imgO(290,276,R) >= red_min && imgO(290,276,R) <= red_max) && ...
+            (imgO(289,276,R) >= red_min && imgO(289,276,R) <= red_max) && ...
+            (imgO(288,276,R) >= red_min && imgO(288,276,R) <= red_max) && ...
+            (imgO(287,276,R) >= red_min && imgO(287,276,R) <= red_max) && ...
+            (imgO(286,276,R) >= red_min && imgO(286,276,R) <= red_max) && ...
+            (imgO(285,277,R) >= red_min && imgO(285,277,R) <= red_max) && ...
+            (imgO(284,277,R) >= red_min && imgO(284,277,R) <= red_max) && ...
+            (imgO(283,277,R) >= red_min && imgO(283,277,R) <= red_max) && ...
+            (imgO(282,277,R) >= red_min && imgO(282,277,R) <= red_max) && ...
+            (imgO(281,278,R) >= red_min && imgO(281,278,R) <= red_max) )
+    
+            %segura botao
+            holding_buttons('red') = true;
+            holding_times('red') = tic;
+            
+            fprintf(galileo,'%c', APERTA_SEM_SOLTAR_RED);  
+        end
+    
+        %quando o rastro acaba solta
+        %Se esta_apertando e nao ha mais rastro passando
+        if( holding_buttons('red') && ...
+            ~(imgO(311,274,R) >= red_min && imgO(311,274,R) <= red_max) && ...
+            toc(holding_times('red')) > tempo_espera)
+    
+            holding_buttons('red') = false;
+    
+            fprintf(galileo,'%c', SOLTA_RED);  
+    
+        end
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% rastro_play %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
         %detect red   
         if( redPixel >= red_min && redPixel <= red_max && ...
