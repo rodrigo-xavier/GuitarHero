8da14181888ebc75e780dda4276f3b3eb702e044
rodrigo-xavier <rodrigomatiasxavier@gmail.com>
2019-08-16 19:50:58 -0300

    Inserindo novas condições para pegar os tempos

--- src/detect_times.m
+++ src/detect_times.m
@@ -2,15 +2,15 @@ function [note_time, trail_time] = detect_times(vid)
     disp("Detectando os tempos.");
 
     % Define number of samples
-    note_max = 20;
+    note_max        = 20;
 
     % Initialize zero
-    queue_trail = {};
-    queue_note = {};
-    temp = {};
-    timer_of_trail = 0;
-    timer_of_note = 0;
-    note = 0;
+    queue_trail     = {};
+    queue_note      = {};
+    temp            = {};
+    timer_of_trail  = 0;
+    timer_of_note   = 0;
+    note            = 0;
 
     % Initialize patterns
     R               = 1;
@@ -34,63 +34,66 @@ function [note_time, trail_time] = detect_times(vid)
     orangeG_min     = 175;
     orangeG_max     = 255;
 
+    % Flags
+    counter     = 0;
+
     while (note < note_max)
         imgO = getdata(vid,1,'uint8');
 
         % Detecta pixels 1 e 2 com diferença de 6 pixels 
         % para garantir que existe uma única nota, e não 6 notas
 
-        greenPixelUp1        = imgO(287,238,G);
+        greenPixelUp        = imgO(287,238,G);
         greenPixelUp2        = imgO(293,238,G);
-        greenPixelMiddle1    = imgO(306,230,G);
+        greenPixelMiddle    = imgO(306,230,G);
         greenPixelMiddle2    = imgO(312,230,G);
-        greenPixelDown1      = imgO(406,185,G);
+        greenPixelDown      = imgO(406,185,G);
         greenPixelDown2      = imgO(412,185,G);
 
-        redPixelUp1          = imgO(287,275,R);
+        redPixelUp          = imgO(287,275,R);
         redPixelUp2          = imgO(293,275,R);
-        redPixelMiddle1      = imgO(305,274,R);
+        redPixelMiddle      = imgO(305,274,R);
         redPixelMiddle2      = imgO(311,274,R);
-        redPixelDown1        = imgO(405,250,R);
+        redPixelDown        = imgO(405,250,R);
         redPixelDown2        = imgO(411,250,R);
 
-        yellowPixelRUp1      = imgO(287,312,R);
+        yellowPixelRUp      = imgO(287,312,R);
         yellowPixelRUp2      = imgO(293,312,R);
-        yellowPixelGUp1      = imgO(287,312,G);
+        yellowPixelGUp      = imgO(287,312,G);
         yellowPixelGUp2      = imgO(293,312,G);
-        yellowPixelRMiddle1  = imgO(306,311,R);
+        yellowPixelRMiddle  = imgO(306,311,R);
         yellowPixelRMiddle2  = imgO(312,311,R);
-        yellowPixelGMiddle1  = imgO(306,311,G);
+        yellowPixelGMiddle  = imgO(306,311,G);
         yellowPixelGMiddle2  = imgO(312,311,G);
-        yellowPixelRDown1    = imgO(405,313,R);
+        yellowPixelRDown    = imgO(405,313,R);
         yellowPixelRDown2    = imgO(411,313,R);
-        yellowPixelGDown1    = imgO(405,313,G);
+        yellowPixelGDown    = imgO(405,313,G);
         yellowPixelGDown2    = imgO(411,313,G);
 
-        bluePixelGUp1        = imgO(287,349,G);
+        bluePixelGUp        = imgO(287,349,G);
         bluePixelGUp2        = imgO(293,349,G);
-        bluePixelBUp1        = imgO(287,349,B);
+        bluePixelBUp        = imgO(287,349,B);
         bluePixelBUp2        = imgO(293,349,B);
-        bluePixelGMiddle1    = imgO(306,354,G);
+        bluePixelGMiddle    = imgO(306,354,G);
         bluePixelGMiddle2    = imgO(312,354,G);
-        bluePixelBMiddle1    = imgO(306,354,B);
+        bluePixelBMiddle    = imgO(306,354,B);
         bluePixelBMiddle2    = imgO(312,354,B);
-        bluePixelGDown1      = imgO(405,376,G);
+        bluePixelGDown      = imgO(405,376,G);
         bluePixelGDown2      = imgO(411,376,G);
-        bluePixelBDown1      = imgO(405,376,B);
+        bluePixelBDown      = imgO(405,376,B);
         bluePixelBDown2      = imgO(411,376,B);
 
-        orangePixelRUp1      = imgO(287,387,R);
+        orangePixelRUp      = imgO(287,387,R);
         orangePixelRUp2      = imgO(293,387,R);
-        orangePixelGUp1      = imgO(287,387,G);
+        orangePixelGUp      = imgO(287,387,G);
         orangePixelGUp2      = imgO(293,387,G);
-        orangePixelRMiddle1  = imgO(305,395,R);
+        orangePixelRMiddle  = imgO(305,395,R);
         orangePixelRMiddle2  = imgO(311,395,R);
-        orangePixelGMiddle1  = imgO(305,395,G);
+        orangePixelGMiddle  = imgO(305,395,G);
         orangePixelGMiddle2  = imgO(311,395,G);
-        orangePixelRDown1    = imgO(405,440,R);
+        orangePixelRDown    = imgO(405,440,R);
         orangePixelRDown2    = imgO(411,440,R);
-        orangePixelGDown1    = imgO(405,440,G);
+        orangePixelGDown    = imgO(405,440,G);
         orangePixelGDown2    = imgO(411,440,G);
 
         % Se passar um rastro em qualquer trilha, então fudeu, apenas inicie do zero
@@ -207,86 +210,182 @@ function [note_time, trail_time] = detect_times(vid)
             temp = {};
 
         else
+            flag_green_Up = (greenPixelUp >= green_min && greenPixelUp <= green_max).*true + false;
+            flag_red_Up = (redPixelUp >= red_min && redPixelUp <= red_max).*true + false;
+            flag_yellow_Up = (yellowPixelRUp >= yellowR_min && yellowPixelRUp <= yellowR_max && yellowPixelGUp >= yellowG_min && yellowPixelGUp <= yellowG_max).*true + false;
+            flag_blue_Up = (bluePixelGUp >= blueR_min && bluePixelGUp <= blueR_max && bluePixelBUp >= blueG_min && bluePixelBUp <= blueG_max).*true + false;
+            flag_orange_Up = (orangePixelRUp >= orangeR_min && orangePixelRUp <= orangeR_max && orangePixelGUp >= orangeG_min && orangePixelGUp <= orangeG_max).*true + false;
 
-            % Detect Note UP and push to queue
-            if(greenPixelUp1 >= green_min    && greenPixelUp1   <= green_max   && ...
-            greenPixelUp2    >= green_min    && greenPixelUp2   <= green_max   || ...
-            redPixelUp1      >= red_min      && redPixelUp1     <= red_max     && ...
-            redPixelUp2      >= red_min      && redPixelUp2     <= red_max     || ...
-            yellowPixelRUp1  >= yellowR_min  && yellowPixelRUp1 <= yellowR_max && ...
-            yellowPixelRUp2  >= yellowR_min  && yellowPixelRUp2 <= yellowR_max && ...
-            yellowPixelGUp1  >= yellowG_min  && yellowPixelGUp1 <= yellowG_max && ...
-            yellowPixelGUp2  >= yellowG_min  && yellowPixelGUp2 <= yellowG_max || ...
-            bluePixelGUp1    >= blueG_min    && bluePixelGUp1   <= blueG_max   && ...
-            bluePixelGUp2    >= blueG_min    && bluePixelGUp2   <= blueG_max   && ...
-            bluePixelBUp1    >= blueB_min    && bluePixelBUp1   <= blueB_max   && ...
-            bluePixelBUp2    >= blueB_min    && bluePixelBUp2   <= blueB_max   || ...
-            orangePixelRUp1  >= orangeR_min  && orangePixelRUp1 <= orangeR_max && ...
-            orangePixelRUp2  >= orangeR_min  && orangePixelRUp2 <= orangeR_max && ...
-            orangePixelGUp1  >= orangeG_min  && orangePixelGUp1 <= orangeG_max && ...
-            orangePixelGUp2  >= orangeG_min  && orangePixelGUp2 <= orangeG_max )
-                
-                % Push trail
+            if(flag_green_Up && ~(greenPixelUp >= green_min && greenPixelUp <= green_max))
                 queue_trail{end + 1} = tic;
-
+                flag_green_Up = false;
             end
-        
-        try
-                % 
-                if((greenPixelMiddle1   >= green_min    && greenPixelMiddle1     <= green_max    && ...
-                greenPixelMiddle2    >= green_min    && greenPixelMiddle2     <= green_max    || ...
-                redPixelMiddle1      >= red_min      && redPixelMiddle1       <= red_max      && ...
-                redPixelMiddle2      >= red_min      && redPixelMiddle2       <= red_max      || ...
-                yellowPixelRMiddle1  >= yellowR_min  && yellowPixelRMiddle1   <= yellowR_max  && ...
-                yellowPixelRMiddle2  >= yellowR_min  && yellowPixelRMiddle2   <= yellowR_max  && ...
-                yellowPixelGMiddle1  >= yellowG_min  && yellowPixelGMiddle1   <= yellowG_max  && ...
-                yellowPixelGMiddle2  >= yellowG_min  && yellowPixelGMiddle2   <= yellowG_max  || ...
-                bluePixelGMiddle1    >= blueG_min    && bluePixelGMiddle1     <= blueG_max    && ...
-                bluePixelGMiddle2    >= blueG_min    && bluePixelGMiddle2     <= blueG_max    && ...
-                bluePixelBMiddle1    >= blueB_min    && bluePixelBMiddle1     <= blueB_max    && ...
-                bluePixelBMiddle2    >= blueB_min    && bluePixelBMiddle2     <= blueB_max    || ...
-                orangePixelRMiddle1  >= orangeR_min  && orangePixelRMiddle1   <= orangeR_max  && ...
-                orangePixelRMiddle2  >= orangeR_min  && orangePixelRMiddle2   <= orangeR_max  && ...
-                orangePixelGMiddle1  >= orangeG_min  && orangePixelGMiddle1   <= orangeG_max  && ...
-                orangePixelGMiddle2  >= orangeG_min  && orangePixelGMiddle2   <= orangeG_max  ))
+            if(flag_red_Up && ~(redPixelUp >= red_min && redPixelUp <= red_max))
+                queue_trail{end + 1} = tic;
+                flag_red_Up = false;
+            end
+            if(flag_yellow_Up && 
+            ~(yellowPixelRUp >= yellowR_min && yellowPixelRUp <= yellowR_max && 
+            yellowPixelGUp >= yellowG_min && yellowPixelGUp <= yellowG_max))
+                queue_trail{end + 1} = tic;
+                flag_yellow_Up = false;
+            end
+            if(flag_blue_Up && 
+            ~(bluePixelGUp >= blueR_min && bluePixelGUp <= blueR_max && 
+            bluePixelBUp >= blueG_min && bluePixelBUp <= blueG_max)) ||
+                queue_trail{end + 1} = tic;
+                flag_blue_Up = false;
+            end
+            if(flag_orange_Up && 
+            ~(orangePixelRUp >= orangeR_min && orangePixelRUp <= orangeR_max && 
+            orangePixelGUp >= orangeG_min && orangePixelGUp <= orangeG_max))
+                queue_trail{end + 1} = tic;
+                flag_orange_Up = false;
+            end
+            
+            flag_green_middle = (greenPixelMiddle >= green_min && greenPixelMiddle <= green_max).*true + false;
+            flag_red_middle = (redPixelMiddle >= red_min && redPixelMiddle <= red_max).*true + false;
+            flag_yellow_middle = (yellowPixelRMiddle >= yellowR_min && yellowPixelRMiddle <= yellowR_max && yellowPixelGMiddle >= yellowG_min && yellowPixelGMiddle <= yellowG_max).*true + false;
+            flag_blue_middle = (bluePixelGMiddle >= blueR_min && bluePixelGMiddle <= blueR_max && bluePixelBMiddle >= blueG_min && bluePixelBMiddle <= blueG_max).*true + false;
+            flag_orange_middle = (orangePixelRMiddle >= orangeR_min && orangePixelRMiddle <= orangeR_max && orangePixelGMiddle >= orangeG_min && orangePixelGMiddle <= orangeG_max).*true + false;
 
-                    % Push time trail
-                    temp{end + 1} = toc(queue_trail{1});
-                    % Pop trail
-                    queue_trail = queue_trail(2:end);
-                    % Push note
-                    queue_note{end + 1} = tic;
-                end
+            if(flag_green_middle && ~(greenPixelMiddle >= green_min && greenPixelMiddle <= green_max))
+                % Push time trail
+                temp{end + 1} = toc(queue_trail{1});
+                % Pop trail
+                queue_trail = queue_trail(2:end);
+                % Push note
+                queue_note{end + 1} = tic;
 
-                % 
-                if((greenPixelDown1   >= green_min    && greenPixelDown1     <= green_max    && ...
-                greenPixelDown2    >= green_min    && greenPixelDown2     <= green_max    || ...
-                redPixelDown1      >= red_min      && redPixelDown1       <= red_max      && ...
-                redPixelDown2      >= red_min      && redPixelDown2       <= red_max      || ...
-                yellowPixelRDown1  >= yellowR_min  && yellowPixelRDown1   <= yellowR_max  && ...
-                yellowPixelRDown2  >= yellowR_min  && yellowPixelRDown2   <= yellowR_max  && ...
-                yellowPixelGDown1  >= yellowG_min  && yellowPixelGDown1   <= yellowG_max  && ...
-                yellowPixelGDown2  >= yellowG_min  && yellowPixelGDown2   <= yellowG_max  || ...
-                bluePixelGDown1    >= blueG_min    && bluePixelGDown1     <= blueG_max    && ...
-                bluePixelGDown2    >= blueG_min    && bluePixelGDown2     <= blueG_max    && ...
-                bluePixelBDown1    >= blueB_min    && bluePixelBDown1     <= blueB_max    && ...
-                bluePixelBDown2    >= blueB_min    && bluePixelBDown2     <= blueB_max    || ...
-                orangePixelRDown1  >= orangeR_min  && orangePixelRDown1   <= orangeR_max  && ...
-                orangePixelRDown2  >= orangeR_min  && orangePixelRDown2   <= orangeR_max  && ...
-                orangePixelGDown1  >= orangeG_min  && orangePixelGDown1   <= orangeG_max  && ...
-                orangePixelGDown2  >= orangeG_min  && orangePixelGDown2   <= orangeG_max  ))
-                    
-                    % Calculate time trail
-                    timer_of_trail = temp{1} + timer_of_trail;
-                    % Calculate time note
-                    timer_of_note = toc(queue_note{1}) + timer_of_note;
-                    % Pop time trail
-                    temp = temp(2:end);
-                    % Pop note
-                    queue_note = queue_note(2:end);
-                    % Counter while
-                    note = note + 1;
-                end
+                flag_green_middle = false;
+            end
+
+            if(flag_red_middle && ~(redPixelMiddle >= red_min && redPixelMiddle <= red_max))
+                % Push time trail
+                temp{end + 1} = toc(queue_trail{1});
+                % Pop trail
+                queue_trail = queue_trail(2:end);
+                % Push note
+                queue_note{end + 1} = tic;
+
+                flag_red_middle = false;
+            end
+
+            if(flag_yellow_middle && 
+            ~(yellowPixelRMiddle >= yellowR_min && yellowPixelRMiddle <= yellowR_max && 
+            yellowPixelGMiddle >= yellowG_min && yellowPixelGMiddle <= yellowG_max))
+                % Push time trail
+                temp{end + 1} = toc(queue_trail{1});
+                % Pop trail
+                queue_trail = queue_trail(2:end);
+                % Push note
+                queue_note{end + 1} = tic;
+
+                flag_yellow_middle = false;
+            end
+
+            if(flag_blue_middle && 
+            ~(bluePixelGMiddle >= blueR_min && bluePixelGMiddle <= blueR_max && 
+            bluePixelBMiddle >= blueG_min && bluePixelBMiddle <= blueG_max)) ||
+                % Push time trail
+                temp{end + 1} = toc(queue_trail{1});
+                % Pop trail
+                queue_trail = queue_trail(2:end);
+                % Push note
+                queue_note{end + 1} = tic;
+
+                flag_blue_middle = false;
+            end
+
+            if(flag_orange_middle && 
+            ~(orangePixelRMiddle >= orangeR_min && orangePixelRMiddle <= orangeR_max && 
+            orangePixelGMiddle >= orangeG_min && orangePixelGMiddle <= orangeG_max))
+                % Push time trail
+                temp{end + 1} = toc(queue_trail{1});
+                % Pop trail
+                queue_trail = queue_trail(2:end);
+                % Push note
+                queue_note{end + 1} = tic;
+
+                flag_orange_middle = false;
+            end
+
+            flag_green_Down = (greenPixelDown >= green_min && greenPixelDown <= green_max).*true + false;
+            flag_red_Down = (redPixelDown >= red_min && redPixelDown <= red_max).*true + false;
+            flag_yellow_Down = (yellowPixelRDown >= yellowR_min && yellowPixelRDown <= yellowR_max && yellowPixelGDown >= yellowG_min && yellowPixelGDown <= yellowG_max).*true + false;
+            flag_blue_Down = (bluePixelGDown >= blueR_min && bluePixelGDown <= blueR_max && bluePixelBDown >= blueG_min && bluePixelBDown <= blueG_max).*true + false;
+            flag_orange_Down = (orangePixelRDown >= orangeR_min && orangePixelRDown <= orangeR_max && orangePixelGDown >= orangeG_min && orangePixelGDown <= orangeG_max).*true + false;
+
+            if(flag_green_Down && ~(greenPixelDown >= green_min && greenPixelDown <= green_max))
+                % Calculate time trail
+                timer_of_trail = temp{1} + timer_of_trail;
+                % Calculate time note
+                timer_of_note = toc(queue_note{1}) + timer_of_note;
+                % Pop time trail
+                temp = temp(2:end);
+                % Pop note
+                queue_note = queue_note(2:end);
+                % Counter while
+                note = note + 1;
+                flag_green_Down = false;
+            end
+            if(flag_red_Down && ~(redPixelDown >= red_min && redPixelDown <= red_max))
+                % Calculate time trail
+                timer_of_trail = temp{1} + timer_of_trail;
+                % Calculate time note
+                timer_of_note = toc(queue_note{1}) + timer_of_note;
+                % Pop time trail
+                temp = temp(2:end);
+                % Pop note
+                queue_note = queue_note(2:end);
+                % Counter while
+                note = note + 1;
+                flag_red_Down = false;
+            end
+            if(flag_yellow_Down && 
+            ~(yellowPixelRDown >= yellowR_min && yellowPixelRDown <= yellowR_max && 
+            yellowPixelGDown >= yellowG_min && yellowPixelGDown <= yellowG_max))
+                % Calculate time trail
+                timer_of_trail = temp{1} + timer_of_trail;
+                % Calculate time note
+                timer_of_note = toc(queue_note{1}) + timer_of_note;
+                % Pop time trail
+                temp = temp(2:end);
+                % Pop note
+                queue_note = queue_note(2:end);
+                % Counter while
+                note = note + 1;
+                flag_yellow_Down = false;
+            end
+            if(flag_blue_Down && 
+            ~(bluePixelGDown >= blueR_min && bluePixelGDown <= blueR_max && 
+            bluePixelBDown >= blueG_min && bluePixelBDown <= blueG_max)) ||
+                % Calculate time trail
+                timer_of_trail = temp{1} + timer_of_trail;
+                % Calculate time note
+                timer_of_note = toc(queue_note{1}) + timer_of_note;
+                % Pop time trail
+                temp = temp(2:end);
+                % Pop note
+                queue_note = queue_note(2:end);
+                % Counter while
+                note = note + 1;
+                flag_blue_Down = false;
+            end
+            if(flag_orange_Down && 
+            ~(orangePixelRDown >= orangeR_min && orangePixelRDown <= orangeR_max && 
+            orangePixelGDown >= orangeG_min && orangePixelGDown <= orangeG_max))
+                % Calculate time trail
+                timer_of_trail = temp{1} + timer_of_trail;
+                % Calculate time note
+                timer_of_note = toc(queue_note{1}) + timer_of_note;
+                % Pop time trail
+                temp = temp(2:end);
+                % Pop note
+                queue_note = queue_note(2:end);
+                % Counter while
+                note = note + 1;
+                flag_orange_Down = false;
             end
         end
     end
