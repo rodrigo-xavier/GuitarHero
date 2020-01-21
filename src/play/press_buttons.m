e1d9546049ad192baefa46d7ad91c805c0d84c75
rodrigo.xavier <rodrigomatiasxavier@gmail.com>
2020-01-21 09:29:23 -0300

    consertando erros

--- src/play/press_buttons.m
+++ src/play/press_buttons.m
@@ -3,7 +3,7 @@ function press_buttons(video, arduino)
     playgame = Play(arduino);
 
     while true
-        playgame.get_pixels();
+        playgame.get_pixels(video);
         playgame.set_up();
         playgame.tear_down();
     end
