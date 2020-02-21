5c2a83742696e1cb9c323556381f92df1a1e6e9d
rodrigo.xavier <rodrigomatiasxavier@gmail.com>
2020-02-21 17:54:19 -0300

    saving progress

--- src/play/press_buttons.m
+++ src/play/press_buttons.m
@@ -1,9 +1,25 @@
 function press_buttons(video, arduino)
 
-    playgame = Play(arduino);
+    % cores
+    R = 1;
+    G = 2;
+    B = 3;
+
+    playgame = Playgame();
+    playgame.arduino = arduino;
 
     while true
-        playgame.get_pixels(video);
+        imgO = getdata(video,1,'uint8');
+
+        playgame.greenPixel     = imgO(312,230,playgame.G);
+        playgame.redPixel       = imgO(311,274,playgame.R);
+        playgame.yellowPixelR   = imgO(312,311,playgame.R);
+        playgame.yellowPixelG   = imgO(312,311,playgame.G);
+        playgame.bluePixelG     = imgO(312,354,playgame.G);
+        playgame.bluePixelB     = imgO(312,354,playgame.B);
+        playgame.orangePixelR   = imgO(311,395,playgame.R);
+        playgame.orangePixelG   = imgO(311,395,playgame.G);
+
         playgame.set_up();
         playgame.tear_down();
     end
